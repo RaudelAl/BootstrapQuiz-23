f68aaa3c8996421fa6c82d691641053e5d76f260
RaudelAl <chache5445@icloud.com>
2023-11-06 09:36:42 -0800

    hello

---
+++ spripts/app.js
@@ -0,0 +1,9 @@
+
+let btnChange1 = document.getElementById("btnChange1");
+
+let affectme1 = document.getElementById("affectme1");
+
+btnChange1.addEventListener("click", function(e){
+
+    affectme1.className = "spinner";
+});
